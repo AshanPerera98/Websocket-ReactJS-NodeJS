d3d4d9ff70665aa72e72445db6e2be15ebfe37bd
Ashan Perera <ashantiwankaperera@gmail.com>
2024-02-24 17:23:40 +0530

    server complete

--- server/index.js
+++ server/index.js
@@ -5,6 +5,50 @@ const server = http.createServer();
 const wsServer = new WebSocketServer({ server });
 const port = 8080;
 
+const url = require("url");
+const uuidv4 = require("uuid").v4;
+
+const connections = {};
+const users = {};
+
+wsServer.on("connection", (connection, request) => {
+  const { username } = url.parse(request.url, true).query;
+  const uuid = uuidv4();
+
+  connections[uuid] = connection;
+  users[uuid] = { username, state: {} };
+
+  connection.on("message", (message) => handleMessage(message, uuid));
+  connection.on("close", () => handleClose(uuid));
+});
+
+const handleMessage = (bytes, uuid) => {
+  const message = JSON.parse(bytes.toString());
+  const user = users[uuid];
+
+  user.state = message;
+  boradcastUsers();
+
+  console.log(`${user.username} updated state: ${JSON.stringify(user.state)}`);
+};
+
+const handleClose = (uuid) => {
+  console.log(`${users[uuid].username} disconnected`);
+
+  delete connections[uuid];
+  delete users[uuid];
+
+  boradcastUsers();
+};
+
+const boradcastUsers = () => {
+  Object.keys(connections).forEach((uuid) => {
+    const connection = connections[uuid];
+    const message = JSON.stringify(users);
+    connection.send(message);
+  });
+};
+
 server.listen(port, () => {
   console.log(`WebSocket server running on port ${port}`);
 });
