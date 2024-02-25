0afb74a8c7cc9267d9f724f8512501bd30fba587
Ashan Perera <ashantiwankaperera@gmail.com>
2024-02-25 12:22:34 +0530

    cursor coordinates send to ws added

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,4 +1,24 @@
+import { useEffect, useRef } from "react";
+import useWebSocket from "react-use-websocket";
+import throttle from "lodash.throttle";
+
 function Home({ username }) {
+  const WS_URL = "ws://127.0.0.1:8080";
+  const { sendJsonMessage } = useWebSocket(WS_URL, {
+    queryParams: { username },
+  });
+
+  const sendJsonMessageThrottled = useRef(throttle(sendJsonMessage, 100));
+
+  useEffect(() => {
+    window.addEventListener("mousemove", (e) => {
+      sendJsonMessageThrottled.current({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    });
+  }, []);
+
   return <h1>Welcome! {username}</h1>;
 }
 
